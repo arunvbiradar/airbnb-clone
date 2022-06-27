3b55693923762f68d670870f1b417a30eba44430
applikers <dev.applikers@gmail.com>
2022-06-28 01:05:52 +0530

    Initiail commit

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+// App config
+const app = express();
+
+// Middleware
+app.use(express());
+
+// DB Config
+
+// API endpoint
+
+// Listener
+app.listen(5000, () => console.log("server started"));
